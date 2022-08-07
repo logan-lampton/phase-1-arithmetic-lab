da181940dc66dbfe5036ce130bca0231f08ad118
Logan Lampton <lampton.logan@gmail.com>
2022-08-07 13:10:20 -0400

    commit tree 8a10908b3324c8a65cc142b8277f1f3b1c308664

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random =  Math.floor(Math.random()) + 1;
+console.log(random);
+
+let num3 = 34
+let num4 = 5
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 4, 5, 20);
